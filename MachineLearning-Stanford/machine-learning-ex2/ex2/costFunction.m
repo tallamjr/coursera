c49478f4b5bac88ca2f81bcad26bfe63a5d6c388
Tarek Allam <t.allam.jr@gmail.com>
2016-11-21 23:33:20 +0000

    Week 2 assignment complete up to prediction section

--- MachineLearning-Stanford/machine-learning-ex2/ex2/costFunction.m
+++ MachineLearning-Stanford/machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -19,6 +19,18 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+
+
+z = X*theta;
+
+h = sigmoid(z);
+
+J = 1 / m * sum(-y.*log(h) - (1 - y).*log(1 - h));
+
+for i = 1:size(theta, 1)
+    grad(i) = 1 / m * sum((h - y) .* X(:, i));
+end
+
 
 
 
